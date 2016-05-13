fb713290f5cd92ab23c5ad1f9a694179ce41d004
Gabriel Oliveira <barbosa.olivera1@gmail.com>
2016-05-12 21:10:10 -0300

    Comit  do servico de usuario.
    Implementação dos metodos do login e logout.

--- Código Fonte/app/Services/WebServiceUsuarios.ts
+++ Código Fonte/app/Services/WebServiceUsuarios.ts
@@ -1,8 +1,20 @@
 import { GenericDAO }      from './GenericDAO';
 import { Usuarios   }      from './../Models/Usuarios';
 import { Injectable }      from '@angular/core';
+import {Subscriber}     from 'rxjs/Subscriber';
+import {Http, Response} from '@angular/http';
 
 @Injectable()
 export class WebSerivceUsuarios extends GenericDAO{
-        
+    
+    login( usuario: Usuarios, subscriber: Subscriber<Response>){
+        this._urlFind = "";
+        this.find( usuario, subscriber);
+    }
+    
+    logout( usuario: Usuarios, subscriber: Subscriber<Response>){
+        this._urlFind = "";
+        this.find(usuario, subscriber);
+    }
+    
 }
